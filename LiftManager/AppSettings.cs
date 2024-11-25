6623ff4b8bed09c70c8391e4c394745506e49d76
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-11-25 16:51:40 -0600

    added number of floors config attribute to AppSettings

--- LiftManager/AppSettings.cs
+++ LiftManager/AppSettings.cs
@@ -3,9 +3,11 @@ namespace LiftManager;
 public class AppSettings : IAppSettings
 {
   public required int InitialFloor { get; set; }
+  public required int NumberOfFloors { get; set; }
 }
 
 public interface IAppSettings
 {
   int InitialFloor { get; set; }
+  int NumberOfFloors { get; set; }
 }
