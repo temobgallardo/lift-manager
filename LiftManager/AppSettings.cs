199a92a8096b6ca2b823a0f3323e39435acc7f9d
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-11-28 20:15:37 -0600

    added two settings

--- LiftManager/AppSettings.cs
+++ LiftManager/AppSettings.cs
@@ -3,11 +3,15 @@ namespace LiftManager;
 public class AppSettings : IAppSettings
 {
   public required int InitialFloor { get; set; }
+  public required int FilesForBulk { get; set; }
   public required int NumberOfFloors { get; set; }
+  public required string DatabaseDirectory { get; set; }
 }
 
 public interface IAppSettings
 {
   int InitialFloor { get; set; }
+  int FilesForBulk { get; set; }
   int NumberOfFloors { get; set; }
+  string DatabaseDirectory { get; set; }
 }
