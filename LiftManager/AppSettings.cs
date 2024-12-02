46858d1f334eaf1b8e5c4ef6892eaf0eca1f3453
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-12-01 18:47:42 -0600

    added TravelSimulationTime setting

--- LiftManager/AppSettings.cs
+++ LiftManager/AppSettings.cs
@@ -5,6 +5,7 @@ public class AppSettings : IAppSettings
   public required int InitialFloor { get; set; }
   public required int FilesForBulk { get; set; }
   public required int NumberOfFloors { get; set; }
+  public required int TravelSimulationTime { get; set; }
   public required string DatabaseDirectory { get; set; }
 }
 
@@ -13,5 +14,11 @@ public interface IAppSettings
   int InitialFloor { get; set; }
   int FilesForBulk { get; set; }
   int NumberOfFloors { get; set; }
+  /// <summary>
+  /// The time the lift travel from floor to floor in milliseconds
+  /// </summary>
+  int TravelSimulationTime { get; set; }
+
   string DatabaseDirectory { get; set; }
+
 }
