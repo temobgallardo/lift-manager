a24a7121ef3670c522cd4f0477f580d6a7991332
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2024-12-02 14:35:57 -0600

    added compile conditioners for testing

--- LiftManager/Workers/LiftManagerWorker.cs
+++ LiftManager/Workers/LiftManagerWorker.cs
@@ -1,11 +1,11 @@
 using LiftManager.Core;
-using LiftManager.Domain.Enums;
 
 namespace LiftManager.Workers;
 
 public class LiftManagerWorker : WorkerBase
 {
   private readonly IOperator _liftOperator;
+  private const string ARG_INDEX_KEY = "liftRequest";
 
   public LiftManagerWorker(ILogger<WorkerBase> logger, IConfiguration configuration, IOperator liftOperator) : base(logger, configuration)
   {
@@ -16,41 +16,67 @@ public class LiftManagerWorker : WorkerBase
   {
     Logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
+    // TODO: a request manager on top of the Operator so it can better pass request to it in a more efficient manner (not so linear).
+
 #if DEBUG
-    string[] experment = new[] { "outside", "inside", "outside", "inside", "outside" };
+    // string[] experiment = new[] { "outside=1", "inside=2", "outside=3", "inside=1", "outside=2", "outside=10", "outside=1", "inside=10", "inside=10", "outside=4", "inside=6" };
+
+    string[] experiment = Configuration[ARG_INDEX_KEY].Split(',');
+    foreach (var e in experiment)
+    {
+      var arg = e.Split('=');
+      if (arg[0].Contains("outside"))
+      {
+        await CheckIfOutsideLiftRequestAndOperate(arg[1]);
+      }
+      else
+      {
+        await CheckIfInsideLiftRequestAndOperate(arg[1]);
+      }
+    }
 #else
-    CheckIfOutsideLiftRequestAndOperate();
-    CheckIfInsideLiftRequestAndOperate();
+     string[] experiment = Configuration[ARG_INDEX_KEY].Split(',');
+        foreach (var e in experiment)
+        {
+          var arg = e.Split('=');
+          if (arg[0].Contains("outside"))
+          {
+            await CheckIfOutsideLiftRequestAndOperate(arg[1]);
+          }
+          else
+          {
+            await CheckIfInsideLiftRequestAndOperate(arg[1]);
+          }
+        }
+        await CheckIfOutsideLiftRequestAndOperate(Configuration["outside"]);
+        await CheckIfInsideLiftRequestAndOperate(Configuration["inside"]);
 #endif
+
     Logger.LogInformation("Work completed at: {time}", DateTime.Now);
   }
 
-  private void CheckIfInsideLiftRequestAndOperate()
+  private async Task CheckIfInsideLiftRequestAndOperate(string floor)
   {
-    string[]? args = GetArgs(OperationType.Inside.ToString());
-
-    if (args == null)
+    if (floor == null)
     {
+      Logger.LogInformation($"No operation requested");
       return;
     }
 
-    Logger.LogInformation($"Lift operation requested | Operation: Inside Lift | Move to floor: {args[0]}");
-    _liftOperator.LiftToFloor(Convert.ToInt16(args[0]));
+    Logger.LogInformation($"Lift operation requested | Operation: Inside Lift | Move to floor: {floor}");
+    await _liftOperator.LiftToFloor(Convert.ToInt16(floor));
   }
 
-  private void CheckIfOutsideLiftRequestAndOperate()
+  private async Task CheckIfOutsideLiftRequestAndOperate(string floor)
   {
-    string[]? args = GetArgs(OperationType.Outside.ToString());
-
-    if (args == null)
+    if (floor == null)
     {
+      Logger.LogInformation($"No operation requested");
       return;
     }
 
-    Logger.LogInformation($"Lift operation requested | Operation: Outside Lift | Move to floor: {args[0]}");
+    Logger.LogInformation($"Lift operation requested | Operation: Outside Lift | Move to floor: {floor}");
 
-    _liftOperator.RequestLiftToFloor(Convert.ToInt16(args[0]));
+    await _liftOperator.RequestLiftToFloor(Convert.ToInt16(floor));
   }
-
-  private string[]? GetArgs(string typeOfArgs) => Configuration[typeOfArgs]?.Split(',');
 }
